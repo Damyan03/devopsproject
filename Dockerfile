0b3b0e5fbb62916fbbd7ef341f518072154d95ae
Damyan03 <112628176+Damyan03@users.noreply.github.com>
2023-06-16 21:19:09 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM php:apache
+
+RUN apt-get update \
+    && apt-get install -y libzip-dev \
+    && docker-php-ext-install zip
+
+ENV APACHE_DOCUMENT_ROOT /var/www/html/public
+
+RUN sed -ri -e 's!/var/www/html!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/sites-available/*.conf
+RUN sed -ri -e 's!/var/www/!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
+
+RUN sed -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+RUN a2enmod rewrite
+RUN service apache2 restart
+
+RUN docker-php-ext-install pdo pdo_mysql sockets
+RUN curl -sS https://getcomposer.org/installer​ | php -- \
+     --install-dir=/usr/local/bin --filename=composer
+
+RUN curl -sL https://deb.nodesource.com/setup_20.x | bash -
+RUN apt-get install -y nodejs
+
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+CMD composer install && php artisan migrate --force && php artisan db:seed && apache2-foreground && chown www-data:www-data /var/www/html/storage -R
---
+++ app/Http/Controllers/CatController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cat;
+use Illuminate\Http\Request;
+
+class CatController extends Controller
+{
+    public function index()
+    {
+        $cats = \App\Models\Cat::all();
+
+        return view('cats.index', [
+            'cats' => $cats,
+        ]);
+    }
+    public function show(Cat $cat)
+    {
+        $cat = \App\Models\Cat::findOrFail($cat->id);
+
+        return view('cats.show', [
+            'cat' => $cat,
+        ]);
+    }
+    public function create()
+    {
+        return view('cats.create');
+    }
+    public function store(Request $request)
+    {
+        $cat = new \App\Models\Cat();
+
+        $cat->name = $request->input('name');
+        $cat->color = $request->input('color');
+        $cat->age = $request->input('age');
+
+        $cat->save();
+
+        return redirect()->route('cats.index');
+    }
+    public function edit(Cat $cat)
+    {
+        $cat = Cat::find($cat->id);
+
+        return view('cats.edit', [
+            'cat' => $cat,
+        ]);
+    }
+    public function update(Request $request, $id)
+    {
+        $cat = \App\Models\Cat::findOrFail($id);
+
+        $cat->name = $request->input('name');
+        $cat->color = $request->input('color');
+        $cat->age = $request->input('age');
+
+        $cat->save();
+
+        return redirect()->route('cats.index');
+    }
+    public function destroy($id)
+    {
+        $cat = \App\Models\Cat::findOrFail($id);
+
+        $cat->delete();
+
+        return redirect()->route('cats.index');
+    }
+}
